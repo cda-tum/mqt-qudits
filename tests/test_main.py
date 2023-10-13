48ffbfa90a523df1103748c268123e4454e06e6f
kmato <kevin.mato@tum.de>
2023-10-13 18:27:41 +0200

    check test action
    Signed-off-by: kmato <kevin.mato@tum.de>

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        assert True  # add assertion here
+
+
+if __name__ == "__main__":
+    unittest.main()
