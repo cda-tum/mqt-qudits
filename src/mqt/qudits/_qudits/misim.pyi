0dc22b4ffe22c0b2e0d15d042f76fd24776e14da
burgholzer <burgholzer@me.com>
2024-04-19 21:50:09 +0200

    🚨 run ruff format

--- src/mqt/qudits/_qudits/misim.pyi
+++ src/mqt/qudits/_qudits/misim.pyi
@@ -1,6 +1,5 @@
 from typing import Any
 
-
 # todo: this need significantly better typing and a better docstring
 def state_vector_simulation(circuit: Any, noise_model: dict[str, Any]) -> list[complex]:  # noqa: ANN401
     """Simulate the state vector of a quantum circuit with noise model.
@@ -12,6 +11,5 @@ def state_vector_simulation(circuit: Any, noise_model: dict[str, Any]) -> list[c
     Returns:
         list: The state vector of the quantum circuit
     """
-
 
 __all__ = ["state_vector_simulation"]
