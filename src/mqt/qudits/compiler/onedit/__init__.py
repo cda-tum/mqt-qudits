0d5218cdcb3d7d9403a542a44c6935f77f0b505e
burgholzer <burgholzer@me.com>
2024-04-28 23:55:02 +0200

    🩹 make compilation passes available
    Signed-off-by: burgholzer <burgholzer@me.com>

--- src/mqt/qudits/compiler/onedit/__init__.py
+++ src/mqt/qudits/compiler/onedit/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from .local_phases_transpilation import ZPropagationPass, ZRemovalPass
 from .mapping_aware_transpilation import PhyLocAdaPass, PhyLocQRPass
 from .mapping_un_aware_transpilation import LogLocAdaPass, LogLocQRPass
 
@@ -8,4 +9,6 @@ __all__ = [
     "LogLocQRPass",
     "PhyLocAdaPass",
     "PhyLocQRPass",
+    "ZPropagationPass",
+    "ZRemovalPass",
 ]
