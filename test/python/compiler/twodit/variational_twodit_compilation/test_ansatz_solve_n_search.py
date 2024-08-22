09049eebc582484dde71f12915ca9507e4a7d369
kmato <kevin.mato@tum.de>
2024-08-22 13:17:35 +0200

    fix test

--- test/python/compiler/twodit/variational_twodit_compilation/test_ansatz_solve_n_search.py
+++ test/python/compiler/twodit/variational_twodit_compilation/test_ansatz_solve_n_search.py
@@ -12,7 +12,7 @@ class TestAnsatzSearch(TestCase):
     def test_binary_search_compile(self) -> None:
         self.circuit_og = QuantumCircuit(2, [2, 2], 0)
         cx = self.circuit_og.cx([0, 1])
-        circuit = variational_compile(cx, 1e-2, "MS", 1)
+        circuit = variational_compile(cx, 1e-1, "MS", 2)
         op = mini_unitary_sim(circuit, circuit.instructions)
         f = fidelity_on_unitares(op, cx.to_matrix())
         assert 0 < f < 1
