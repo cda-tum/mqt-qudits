7728e819b1e67623028b7b6d8fbdc01af7eb36c6
Yusuke Kimura <kkyusuke@gmail.com>
2024-10-27 15:10:26 +0100

    Add new noise model

--- src/mqt/qudits/simulation/noise_tools/noise.py
+++ src/mqt/qudits/simulation/noise_tools/noise.py
@@ -10,14 +10,54 @@ class Noise:
     probability_depolarizing: float
     probability_dephasing: float
 
+class SubspaceNoise:
+    """Represents a set of noises for one quantum gate.
+    """
+    probs: dict[{int, int}, Noise] | Noise = None
+
+    def __init__(self, subnoise: Noise | dict[{int, int}, Noise]):
+        self.setNoise(subnoise)
+
+    def __init__(self, probability_depolarizing: float, probability_dephasing: float):
+        noise = Noise(probability_depolarizing, probability_dephasing)
+        self.setNoise(noise)
+
+    def isMathematicalNoise(self) -> bool:
+        return isinstance(self.probs, Noise)
+    
+    def isPysicalNoise(self) -> bool:
+        return isinstance(self.probs, dict[{int, int}, Noise])
+    
+    def getNoise(self, levA: int, levB: int, dim: int) -> Noise:
+        if self.isPhysicalNoise():
+            return self.probs[{levA, levB}]
+        else:
+            return Noise(self.probs.probability_depolarizing * dim / 2, self.probs.probability_dephasing * dim / 2)
+
+    def setNoise(self, noise: Noise):
+        if self.isPhysicalNoise():
+            raise NotImplementedError
+        self.probs = noise
+
+    def setNoise(self, noise: dict[{int, int}, Noise]):
+        if self.isMathematicalNoise():
+            raise NotImplementedError
+        self.probs = noise
+
+    def setNoise(self, noise: Noise, levA: int, levB: int):
+        if self.isMathematicalNoise():
+            raise NotImplementedError
+        if self.probs == None:
+            probs = {}
+        probs[{levA, levB}] = noise
 
 class NoiseModel:
     """Represents a quantum noise model for various gates and qudit configurations."""
 
     def __init__(self) -> None:
-        self.quantum_errors: dict[str, dict[str, Noise]] = {}
+        self.quantum_errors: dict[str, dict[str, SubspaceNoise]] = {}
 
-    def _add_quantum_error(self, noise: Noise, gates: list[str], mode: str) -> None:
+    def _add_quantum_error(self, subnoise: SubspaceNoise, gates: list[str], mode: str) -> None:
         """Helper method to add quantum errors to the model.
 
         Args:
@@ -28,27 +68,27 @@ class NoiseModel:
         for gate in gates:
             if gate not in self.quantum_errors:
                 self.quantum_errors[gate] = {}
-            self.quantum_errors[gate][mode] = noise
+            self.quantum_errors[gate][mode] = subnoise
 
-    def add_quantum_error_locally(self, noise: Noise, gates: list[str]) -> None:
+    def add_quantum_error_locally(self, subnoise: SubspaceNoise, gates: list[str]) -> None:
         """Add a quantum error locally to all qudits for specified gates."""
-        self._add_quantum_error(noise, gates, "local")
+        self._add_quantum_error(subnoise, gates, "local")
 
-    def add_all_qudit_quantum_error(self, noise: Noise, gates: list[str]) -> None:
+    def add_all_qudit_quantum_error(self, subnoise: SubspaceNoise, gates: list[str]) -> None:
         """Add a quantum error to all qudits for specified gates."""
-        self._add_quantum_error(noise, gates, "all")
+        self._add_quantum_error(subnoise, gates, "all")
 
-    def add_nonlocal_quantum_error(self, noise: Noise, gates: list[str]) -> None:
+    def add_nonlocal_quantum_error(self, subnoise: SubspaceNoise, gates: list[str]) -> None:
         """Add a nonlocal quantum error for specified gates."""
-        self._add_quantum_error(noise, gates, "nonlocal")
+        self._add_quantum_error(subnoise, gates, "nonlocal")
 
-    def add_nonlocal_quantum_error_on_target(self, noise: Noise, gates: list[str]) -> None:
+    def add_nonlocal_quantum_error_on_target(self, subnoise: SubspaceNoise, gates: list[str]) -> None:
         """Add a nonlocal quantum error on target qudits for specified gates."""
-        self._add_quantum_error(noise, gates, "target")
+        self._add_quantum_error(subnoise, gates, "target")
 
-    def add_nonlocal_quantum_error_on_control(self, noise: Noise, gates: list[str]) -> None:
+    def add_nonlocal_quantum_error_on_control(self, subnoise: SubspaceNoise, gates: list[str]) -> None:
         """Add a nonlocal quantum error on control qudits for specified gates."""
-        self._add_quantum_error(noise, gates, "control")
+        self._add_quantum_error(subnoise, gates, "control")
 
     @property
     def basis_gates(self) -> list[str]:
@@ -59,6 +99,6 @@ class NoiseModel:
         """Return a string representation of the NoiseModel."""
         info_str = "NoiseModel Info:\n"
         for gate, errors in self.quantum_errors.items():
-            for mode, noise in errors.items():
-                info_str += f"Gate: {gate}, Mode: {mode}, Noise: {noise}\n"
+            for mode, subnoise in errors.items():
+                info_str += f"Gate: {gate}, Mode: {mode}, SubspaceNoise: {subnoise}\n"
         return info_str
