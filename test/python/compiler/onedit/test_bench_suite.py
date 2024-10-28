71e8009156ae1a514e80142854f1e6414703170c
kmato <kevin.mato@tum.de>
2024-10-28 12:33:00 +0100

    benchmark suite

--- test/python/compiler/onedit/test_bench_suite.py
+++ test/python/compiler/onedit/test_bench_suite.py
@@ -6,6 +6,7 @@ import unittest
 
 import numpy as np
 
+from mqt.qudits.compiler.compilation_minitools.naive_unitary_verifier import mini_unitary_sim
 from mqt.qudits.compiler.onedit.randomized_benchmarking.bench_suite import (
     generate_clifford_group,
     get_h_gate,
@@ -15,6 +16,8 @@ from mqt.qudits.compiler.onedit.randomized_benchmarking.bench_suite import (
     matrix_hash,
     save_clifford_group_to_file,
 )
+from mqt.qudits.quantum_circuit import QuantumCircuit, QuantumRegister
+from mqt.qudits.simulation import MQTQuditProvider
 
 
 class TestCliffordGroupGeneration(unittest.TestCase):
@@ -68,3 +71,65 @@ class TestCliffordGroupGeneration(unittest.TestCase):
                 np.testing.assert_array_almost_equal(clifford_group[key], loaded_group[key])
         finally:
             os.unlink(get_package_data_path(filename))
+
+    def test_benching(self):
+        DIM = 3
+
+        clifford_group = generate_clifford_group(DIM, max_length=10)  # What max length?
+        print(f"Generated {len(clifford_group)} gates")
+
+        save_clifford_group_to_file(clifford_group, f"cliffords_{DIM}.dat")
+
+        DIM = 3
+        clifford_group = load_clifford_group_from_file(f"cliffords_{DIM}.dat")
+
+        def create_rb_sequence(length=2):
+            circuit = QuantumCircuit()
+
+            dit_register = QuantumRegister("dits", 1, [3])
+
+            circuit.append(dit_register)
+
+            inversion = np.eye(DIM)
+
+            check = np.eye(DIM)
+
+            for _ in range(length):
+                random_gate = list(clifford_group.values())[np.random.randint(len(clifford_group))]
+                inversion = inversion @ random_gate.conj().T
+                circuit.cu_one(0, random_gate)
+                check = random_gate @ check
+            circuit.cu_one(0, inversion)
+
+            print(f"Number of operations: {len(circuit.instructions)}")
+            print(f"Number of qudits in the circuit: {circuit.num_qudits}")
+            return circuit
+
+        circuit = create_rb_sequence(length=16)
+        print(mini_unitary_sim(circuit, circuit.instructions).round(4))
+
+        compiled_cirucit = circuit.compileO1("faketraps2trits", "adapt")
+
+        uni = mini_unitary_sim(compiled_cirucit, compiled_cirucit.instructions).round(4)
+        v = np.eye(DIM)[:, compiled_cirucit.mappings[0]]
+        v2 = np.eye(DIM)
+        tpuni = uni @ v
+        tpuni = v2.T @ tpuni  # Pi dag
+        print(tpuni)
+
+        print(uni)
+
+        print(f"Number of operations: {len(compiled_cirucit.instructions)}")
+
+        provider = MQTQuditProvider()
+        backend = provider.get_backend("faketraps2trits")
+
+        print(backend.energy_level_graphs[0].edges)
+        print(compiled_cirucit.mappings[0])
+
+        for instruction in compiled_cirucit.instructions:
+            print(instruction.lev_a, instruction.lev_b)
+            print(instruction.theta, instruction.phi)
+        # circuit.simulate()
+
+        # very_crude_backend(compiled_cirucit, "localhost:5173")
